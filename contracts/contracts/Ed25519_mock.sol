ac59507ce2fe5afcd750112205f8e0a7ee9877f0
public-new <public-new@outlook.com>
2024-02-16 22:15:34 +0800

    fixes, formatting, integrate lib

--- contracts/contracts/Ed25519_mock.sol
+++ contracts/contracts/Ed25519_mock.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.8.9;
 
 contract Ed25519 {
-    function scalarMultBase(uint s) public view returns (uint, uint) {
+    function scalarMultBase(uint256 s)
+        external
+        pure
+        returns (uint256, uint256)
+    {
         return (s, s);
     }
-
 }
--- contracts/contracts/Swap.sol
+++ contracts/contracts/Swap.sol
@@ -1,86 +1,100 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.9;
 
-import "./Ed25519_mock.sol";
+import "./Ed25519.sol";
 
 contract Swap {
-  Ed25519 ed25519;
-	// the hash of the expected public key for which the secret `s_b` corresponds.
-	// this public key is a point on the ed25519 curve, and is in 33-byte compressed format (?)
-  bytes32 public pubKeyClaim;
+    // Ed25519 library
+    Ed25519 ed25519;
 
-	// the hash of the expected public key for which the secret `s_a` corresponds.
-	// this public key is a point on the ed25519 curve, and is in 33-byte compressed format (?)
-	bytes32 public pubKeyRefund;
+    // contract creator, Alice
+    address payable owner;
 
-	// time period since contract creation,
-  // when a refund is allowed for Alice before she calls `ready`
-  // (i.e. before Bob locks judecoin)
-	uint256 timeout_0;
+    // the hash of the expected public key for which the secret `s_b` corresponds.
+    // this public key is a point on the ed25519 curve, and is in 33-byte compressed format (?)
+    bytes32 public pubKeyClaim;
 
-  // time period since calling `ready`,
-  // when a refund is allowed for Alice if Bob doesn't claim
-	uint256 timeout_1;
+    // the hash of the expected public key for which the secret `s_a` corresponds.
+    // this public key is a point on the ed25519 curve, and is in 33-byte compressed format (?)
+    bytes32 public pubKeyRefund;
 
-	// contract creator, Alice
-	address payable owner;
+    // time period from contract creation
+    // during which Alice can call either set_ready or refund
+    uint256 public timeout_0;
 
-	// ready is set to true when Alice sees the funds locked on the other chain.
-	// this prevents Bob from withdrawing funds without locking funds on the other chain first
-	bool isReady = false;
+    // time period from the moment Alice calls `set_ready`
+    // during which Bob can claim. After this, Alice can refund again
+    uint256 public timeout_1;
 
-	event DerivedPubKeyClaim(uint256 s);
-	event DerivedPubKeyRefund(uint256 s);
+    // ready is set to true when Alice sees the funds locked on the other chain.
+    // this prevents Bob from withdrawing funds without locking funds on the other chain first
+    bool isReady = false;
 
-	constructor(
-		bytes32 _pubKeyClaim,
-		bytes32 _pubKeyRefund
-	) payable {
-      owner = payable(msg.sender);
-		pubKeyClaim = _pubKeyClaim;
-		pubKeyRefund = _pubKeyRefund;
-		timeout_0 = block.timestamp + 1 days; // TODO: make configurable
-    ed25519 = new Ed25519();
-	}
+    event DerivedPubKeyClaim(uint256 s);
+    event DerivedPubKeyRefund(uint256 s);
 
-	function set_ready() public {
-		require(msg.sender == owner);
-		isReady = true;
-    timeout_1 = block.timestamp + 1 days; // TODO: make configurable
-	}
+    constructor(
+        bytes32 _pubKeyClaim,
+        bytes32 _pubKeyRefund,
+        Ed25519 _ed25519
+    ) payable {
+        owner = payable(msg.sender);
+        pubKeyClaim = _pubKeyClaim;
+        pubKeyRefund = _pubKeyRefund;
+        timeout_0 = block.timestamp + 1 days;
+        ed25519 = _ed25519;
+    }
 
-	function claim(
-		uint256 _s
-	) public {
-      // Bob can claim either when:
-      // Alice never calls ready, and timeout_0 has passed, or
-      // Alice called ready and Bob is within timeframe timeout_1
-      if (isReady == true) {
-          require(block.timestamp <= timeout_1, "Too late to claim!");
-      } else {
-          require(block.timestamp >= timeout_0, "'isReady == false' cannot claim yet!");
-      }
-      /* require((block.timestamp <= timeout_1 && isReady == true) || block.timestamp >= timeout_0, "Cannot claim in the wrong time period!"); */
-		// confirm that provided secret `_s` was used to derive pubKeyClaim
-    (uint px, uint py) = ed25519.scalarMultBase(_s);
-		bytes32 ph = keccak256(abi.encode(px, py));
-    require(ph == pubKeyClaim, "provided secret does not match the expected pubKey");
-		emit DerivedPubKeyClaim(_s);
+    // Alice must call set_ready() within t_0 once she verifies the JUDE has been locked
+    function set_ready() public {
+        require(msg.sender == owner && block.timestamp < timeout_0);
+        isReady = true;
+        timeout_1 = block.timestamp + 1 days;
+    }
 
-		// // send eth to caller
-		payable(msg.sender).transfer(address(this).balance);
-	}
+    // Bob can claim if:
+    // - Alice doesn't call set_ready or refund within t_0, or
+    // - Alice calls ready within t_0, in which case Bob can call claim until t_1
+    function claim(uint256 _s) external {
+        if (isReady == true) {
+            require(block.timestamp < timeout_1, "Too late to claim!");
+        } else {
+            require(
+                block.timestamp >= timeout_0,
+                "'isReady == false' cannot claim yet!"
+            );
+        }
 
-	function refund(
-		uint256 _s
-	) public {
-      require((block.timestamp <= timeout_0 && isReady == false) || block.timestamp <= timeout_1);
+        verifySecret(_s, pubKeyClaim);
+        emit DerivedPubKeyClaim(_s);
 
-      (uint px, uint py) = ed25519.scalarMultBase(_s);
-      bytes32 ph = keccak256(abi.encode(px, py));
-      require(ph == pubKeyRefund, "provided secret does not match the expected pubKey");
-      emit DerivedPubKeyRefund(_s);
+        // send eth to caller (Bob)
+        selfdestruct(payable(msg.sender));
+    }
 
-      // send eth back to owner
-      owner.transfer(address(this).balance);
-	}
+    // Alice can claim a refund:
+    // - Until t_0 unless she calls set_ready
+    // - After t_1, if she called set_ready
+    function refund(uint256 _s) external {
+        require(
+            (!isReady && block.timestamp < timeout_0) ||
+                (isReady && block.timestamp >= timeout_1)
+        );
+
+        verifySecret(_s, pubKeyRefund);
+        emit DerivedPubKeyRefund(_s);
+
+        // send eth back to owner==caller (Alice)
+        selfdestruct(owner);
+    }
+
+    function verifySecret(uint256 _s, bytes32 pubKey) internal view {
+        (uint256 px, uint256 py) = ed25519.derivePubKey(_s);
+        bytes32 ph = keccak256(abi.encode(px, py));
+        require(
+            ph == pubKey,
+            "provided secret does not match the expected pubKey"
+        );
+    }
 }
